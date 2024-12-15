7cd81dc52d831dbfb50f196afa51a6fcc51accec
Gulam Anas <g.anas786@gmail.com>
2024-12-15 23:25:10 +0530

    old changes

--- app.js
+++ app.js
@@ -13,3 +13,36 @@ window.addEventListener("click", (event) => {
   }
 });
 
+// loader wrapper
+
+let spinnerWrapper = document.querySelector('.loader_container');
+window.addEventListener('load', function () {
+  // spinnerWrapper.style.display = 'none'
+  spinnerWrapper.parentElement.removeChild(spinnerWrapper);
+})
+
+navRemove = () => {
+  let navSlide = document.querySelector(".side_nav_wrapper");
+  navSlide.classList.remove("nav_slide");
+  burger.classList.remove("switch");
+};
+
+var navRemove1 = document.querySelector(".nav_remove1");
+navRemove1.addEventListener("click", () => {
+  navRemove();
+});
+
+var navRemove2 = document.querySelector(".nav_remove2");
+navRemove2.addEventListener("click", () => {
+  navRemove();
+});
+
+var navRemove3 = document.querySelector(".nav_remove3");
+navRemove3.addEventListener("click", () => {
+  navRemove();
+});
+
+var navRemove4 = document.querySelector(".nav_remove4");
+navRemove4.addEventListener("click", () => {
+  navRemove();
+});
